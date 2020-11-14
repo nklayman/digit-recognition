dbbecc87bf4ef81fafa61ede2a6e541ec98e5d27
Noah Klayman <noahklayman@gmail.com>
2020-11-14 00:08:09 -0800

    deploy on github pages

---
+++ frontend/deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f https://github.com/nklayman/digit-recognition.git master:gh-pages
+
+cd -
