f7a8a251fab141bc1ef23fdc0707095a6baad8e7
Noah Klayman <noahklayman@gmail.com>
2020-11-14 18:29:08 -0800

    frontend: lint

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,21 +14,21 @@
 </template>
 
 <script lang="ts">
-import { Network } from "rust-neural-network";
-import { defineComponent, onMounted, ref } from "vue";
-import canvas from "./canvas.directive";
-import getBoundingBox from "./getBoundingBox";
+import { Network } from 'rust-neural-network'
+import { defineComponent, onMounted, ref } from 'vue'
+import canvas from './canvas.directive'
+import getBoundingBox from './getBoundingBox'
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   directives: {
-    canvas,
+    canvas
   },
-  setup() {
-    const canvas = ref<HTMLCanvasElement>();
+  setup () {
+    const canvas = ref<HTMLCanvasElement>()
     // Used to scale down input
-    const canvas2 = document.createElement("canvas");
-    const ctx2 = canvas2.getContext("2d");
+    const canvas2 = document.createElement('canvas')
+    const ctx2 = canvas2.getContext('2d')
 
     /**
      * Scales down image drawn on canvas, returns the red pixel values
@@ -36,11 +36,11 @@ export default defineComponent({
      */
     const getData = () => {
       if (!canvas.value) {
-        throw new Error("No canvas");
+        throw new Error('No canvas')
       }
-      const { top, left, width, height } = getBoundingBox(canvas.value);
+      const { top, left, width, height } = getBoundingBox(canvas.value)
 
-      ctx2?.clearRect(0, 0, canvas2.width, canvas2.height);
+      ctx2?.clearRect(0, 0, canvas2.width, canvas2.height)
       ctx2?.drawImage(
         canvas.value as HTMLCanvasElement,
         left,
@@ -51,76 +51,76 @@ export default defineComponent({
         0,
         28,
         28
-      );
-      const alphaValues: number[] = [];
+      )
+      const alphaValues: number[] = []
       ctx2?.getImageData(0, 0, 28, 28).data.forEach((val, i) => {
         if (i % 4 === 3) {
-          alphaValues.push(val / 255);
+          alphaValues.push(val / 255)
         }
-      });
-      return alphaValues;
-    };
+      })
+      return alphaValues
+    }
 
-    const modelLoaded = ref(false);
-    let net: Network;
+    const modelLoaded = ref(false)
+    let net: Network
     /**
      * Loads the wasm code and the weights/biases
      * The weights/biases are prelearned and loaded from the model.json in the public folder
      */
     const loadModel = async () => {
-      const { Network } = await import("rust-neural-network");
-      const model = await (await fetch("./model.json")).text();
-      net = Network.from_model(model);
-      modelLoaded.value = true;
-    };
+      const { Network } = await import('rust-neural-network')
+      const model = await (await fetch('./model.json')).text()
+      net = Network.from_model(model)
+      modelLoaded.value = true
+    }
 
-    const guess = ref();
-    const confidence = ref();
+    const guess = ref()
+    const confidence = ref()
     /**
      * Makes a prediction of what the user drew
      */
     const makePrediction = () => {
-      const input = getData();
-      const predictions = net.predict([input])[0];
-      let ans = 0;
+      const input = getData()
+      const predictions = net.predict([input])[0]
+      let ans = 0
       predictions.forEach((p: number, i: number) => {
         if (p >= predictions[ans]) {
-          ans = i;
-          confidence.value = p.toFixed(3);
+          ans = i
+          confidence.value = p.toFixed(3)
         }
-      });
-      console.log(predictions.map((n: number) => n.toFixed(3)));
-      guess.value = ans;
-    };
+      })
+      console.log(predictions.map((n: number) => n.toFixed(3)))
+      guess.value = ans
+    }
     onMounted(() => {
-      loadModel();
-      canvas.value?.addEventListener("endPaint", () => {
+      loadModel()
+      canvas.value?.addEventListener('endPaint', () => {
         if (modelLoaded.value) {
-          makePrediction();
+          makePrediction()
         }
-      });
-    });
+      })
+    })
 
     /**
      * Clears the canvas and guess/confidence
      */
     const clear = () => {
       canvas.value
-        ?.getContext("2d")
-        ?.clearRect(0, 0, canvas.value.width, canvas.value.height);
-      guess.value = undefined;
-      confidence.value = undefined;
-    };
+        ?.getContext('2d')
+        ?.clearRect(0, 0, canvas.value.width, canvas.value.height)
+      guess.value = undefined
+      confidence.value = undefined
+    }
 
     return {
       clear,
       canvas,
       modelLoaded,
       guess,
-      confidence,
-    };
-  },
-});
+      confidence
+    }
+  }
+})
 </script>
 
 <style>
